a682911d583093275b6f3332d97d6beac1453422
Massimiliano Gasaro <massimiliano.gasaro@atomoshyla.com>
2025-07-03 17:54:11 +0200

    create table in index.html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+import { getList } from "./function.js";
 // per caricare header e footer in modo dinamico
 Promise.all([
     fetch("../header.html").then(res => res.text()),
@@ -8,4 +9,19 @@ Promise.all([
       document.getElementById("footer").innerHTML = footerData;
     })
     .catch(error => console.error("Errore durante il fetch:", error));
+
+getList().forEach(record => {
+    const tableBody = document.querySelector(".styled-table tbody");
+    const row = document.createElement("tr");
+    row.innerHTML = `
+        <td>${record.descrizione}</td>
+        <td>${record.valore.toFixed(2)}</td>
+        <td>${new Date(record.data).toLocaleDateString()}</td>
+        <td>
+            <button class="btn edit-btn">Modifica</button>
+            <button class="btn delete-btn">Cancella</button>
+        </td>
+    `;
+    tableBody.appendChild(row);
+});
 
